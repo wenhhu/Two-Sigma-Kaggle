5eb58b06bd6afc03f083e77a632433146f5c5741
wenhhu <wenhao.app@gmail.com>
2017-11-16 11:16:25 -0500

    minor modification

--- Main-script.py
+++ Main-script.py
@@ -12,16 +12,17 @@ add_na_indicators = True
 add_diff_features = True
 na_indicator_cols = ['technical_9', 'technical_0', 'technical_32', 'technical_16',
                      'technical_38', 'technical_44', 'technical_20', 'technical_30', 'technical_13']
-# allegedly selected by tree-based algorithms
+
 diff_cols = ['technical_22', 'technical_20', 'technical_30', 'technical_13',
-             'technical_34'] # also allegedly selected by tree-based algorithms
+             'technical_34'] 
+
 univar_rlm_cols = ['technical_22', 'technical_20', 'technical_30_d1', 'technical_20_d1',
                    'technical_30', 'technical_13', 'technical_34']
 nr_l2_best_models = 10
 wrlm_quant = 0.99
 wrlm_min_trainset_fraction = 0.9
-wslm_n_covar_sets = 30
-wslm_max_nr_covars_per_set = 2
+wslm_n_group = 30
+wslm_max_feat_per_group = 2
 wslm_max_abs_y = 0.086
 l1_et_n_estimators = 100
 l1_et_max_depth = 4
@@ -32,82 +33,82 @@ rnd_seed = 17
 # Helper functions and objects
 # ========================================================================================
 
-# Stepwise regression: Use greedy algorithm to group features up into n_covar_sets complementary groups.
+# Stepwise regression: Use greedy algorithm to group features up into n_group complementary groups.
 # Each group can contain no more than 2 features
 
 class  stepwise_lm:
-    def __init__(self, n_covar_sets = 30, max_nr_covars_per_set = 2, verbose = True):
-        self.n_covar_sets = n_covar_sets
-        self.max_nr_covars_per_set = max_nr_covars_per_set
+    def __init__(self, n_group = 30, max_feat_per_group = 2, verbose = True):
+        self.n_group = n_group
+        self.max_feat_per_group = max_feat_per_group
         self.verbose = verbose
 
-        if self.max_nr_covars_per_set == None:
-            self.max_nr_covars_per_set = len(train.columns)
-        self.chosen_covar_sets = []
+        if self.max_feat_per_group == None:
+            self.max_feat_per_group = len(train.columns)
+        self.chosen_group = []
         self.models = []
 
     def fit(self, train, y):
         # We initialize the sets such that each one includes a single random unique feature.
 
-        covar_pool = list(train.columns)
-        random.shuffle(covar_pool)
-        for var in covar_pool[:self.n_covar_sets]:
-            self.chosen_covar_sets.append([var])
-        covar_pool = covar_pool[self.n_covar_sets:]
+        group_pool = list(train.columns)
+        random.shuffle(group_pool)
+        for var in group_pool[:self.n_group]:
+            self.chosen_group.append([var])
+        group_pool = group_pool[self.n_group:]
 
         # forward stepwise regression:
         # We go over the set of covariates not chosen initially, and see to which set will
         # adding this new covariate improve the training error the most. We then add this
         # new covariate to that set. When the maximum number of covariates
-        # max_nr_covars_per_set is reached, that set is no longer considered for adding
+        # max_feat_per_group is reached, that set is no longer considered for adding
         # new variables.
 
         # initialize mse by set
-        best_mses_by_set = np.zeros(self.n_covar_sets) + 1e15
+        best_mses_by_set = np.ones(self.n_group) * float('Inf')
 
-        for var in covar_pool:
-            ci = 0
-            # Declare a list to store the mse of covar_sets by adding var
-            mses_per_candidate_covar = []
-            for covar_set in self.chosen_covar_sets:
-                if len(covar_set) < self.max_nr_covars_per_set:
+        for var in group_pool:
+            gi = 0
+            # Declare a list to store the mse of group_sets by adding var
+            mses_per_candidate_group = []
+            for group_set in self.chosen_group:
+                if len(group_set) < self.max_feat_per_group:
                     model = linear_model.LinearRegression(fit_intercept = False,
                                                           normalize = True, copy_X = True, n_jobs = -1)
-                    model.fit(train[covar_set + [var]], y)
-                    mse = metrics.mean_squared_error(y, model.predict(train[covar_set +
+                    model.fit(train[group_set + [var]], y)
+                    mse = metrics.mean_squared_error(y, model.predict(train[group_set +
                                                                             [var]]))
-                    mses_per_candidate_covar.append(mse)
+                    mses_per_candidate_group.append(mse)
                 else:
-                    # If covar_set is full, then keep its original mse
-                    mses_per_candidate_covar.append(best_mses_by_set[ci])
-                ci += 1
+                    # If group_set is full, then keep its original mse
+                    mses_per_candidate_group.append(best_mses_by_set[gi])
+                gi += 1
 
-            gains = best_mses_by_set - mses_per_candidate_covar
+            gains = best_mses_by_set - mses_per_candidate_group
 
             if gains.max() > 0:
                 temp = gains.argmax()
-                # Add var to the covar_set which gain the largest mse improvement
-                self.chosen_covar_sets[temp].append(var)
-                best_mses_by_set[temp] = mses_per_candidate_covar[temp]
+                # Add var to the group_set which gain the largest mse improvement
+                self.chosen_group[temp].append(var)
+                best_mses_by_set[temp] = mses_per_candidate_group[temp]
 
-        csi = 0
-        # Fit linear models with all the covar_sets
-        for covar_set in self.chosen_covar_sets:
+        gsi = 0
+        # Fit linear models with all the group_sets
+        for group_set in self.chosen_group:
             model = linear_model.LinearRegression(fit_intercept = False, normalize = True,
                                                   copy_X = True, n_jobs = -1)
-            model.fit(train[covar_set], y)
+            model.fit(train[group_set], y)
             self.models.append(model)
             if self.verbose:
-                print('Covar set', csi, 'includes', covar_set, 'and achieves',
-                      best_mses_by_set[csi])
-            csi += 1
+                print('Covar set', gsi, 'includes', group_set, 'and achieves',
+                      best_mses_by_set[gsi])
+            gsi += 1
 
     def predict(self, data):
-        csi = 0
-        for covar_set in self.chosen_covar_sets:
-            data['stacked_lm' + str(csi)] = self.models[csi].predict(data[covar_set])
-            csi += 1
-        # Add predictions of all the linear models with different covar_sets to dataframe
+        gsi = 0
+        for group_set in self.chosen_group:
+            data['stacked_lm' + str(gsi)] = self.models[gsi].predict(data[group_set])
+            gsi += 1
+        # Add predictions of all the linear models with different group_sets to dataframe
         return data
 
 # robust regression, set a threshold size for the train data, if training data is less than
@@ -122,7 +123,7 @@ class  robust_lm:
 
     def fit(self, train, y):
         tmp_model = linear_model.Ridge(fit_intercept = False)
-        best_mse = 1e15
+        best_mse = float('Inf')
         better = True
         train_idxs = train.dropna().index
         min_trainset_fraction = len(train) * self.min_trainset_fraction
@@ -204,8 +205,8 @@ train = train.fillna(train_median)
 
 # Use stepwise linear model to fit y against groupped noisy features
 print('Fitting L0 stepwise linear model')
-l0_wslm =  stepwise_lm(n_covar_sets = wslm_n_covar_sets, max_nr_covars_per_set =
-wslm_max_nr_covars_per_set, verbose = True)
+l0_wslm =  stepwise_lm(n_group = wslm_n_group, max_feat_per_group =
+wslm_max_feat_per_group, verbose = True)
 
 # Drop outlying response values from the trainset.
 train_idx = train[abs(train.y) < wslm_max_abs_y].index
@@ -215,7 +216,7 @@ l0_wslm_fitted_and_base_features = l0_wslm.predict(train[base_features])
 l0_wslm_fitted_and_base_features_cols = l0_wslm_fitted_and_base_features.columns
 
 # Use extratree model to fit y against output from stepwise linear model
-print('Training L1 ETs')
+print('Training L1 ExtraTree')
 model = ensemble.ExtraTreesRegressor(n_estimators = l1_et_n_estimators,
                                      max_depth = l1_et_max_depth, n_jobs = -1, random_state = rnd_seed, verbose = 0)
 model.fit(l0_wslm_fitted_and_base_features, train.y)
@@ -235,7 +236,7 @@ l01_columns = l0_columns + l1_columns
 l01_residuals = l0_residuals + l1_residuals
 
 # Model selection layer, which select the optimal model in layer 1 based on their residuals
-print('Training L2 select_best')
+print('Training L2 select top models')
 midxs = np.argmin(np.array(l01_residuals).T, axis = 1)
 midxs = pd.Series(midxs).value_counts().head(nr_l2_best_models).index
 l2_best_models = []
@@ -249,7 +250,7 @@ for midx in midxs:
 l2_best_model_idx = np.argmin(np.array(l2_best_model_residuals).T, axis = 1)
 
 # Fit a extratree classifier to forecast the optimal model based on robust regression's ouput
-print('Training L3 ET')
+print('Training L3 ExtraTree')
 l3_et = ensemble.ExtraTreesClassifier(n_estimators = l3_et_n_estimators,
                                       max_depth = l3_et_max_depth, n_jobs = -1, random_state = rnd_seed, verbose = 0)
 l3_et.fit(l0_wslm_fitted_and_base_features, l2_best_model_idx)
@@ -260,7 +261,7 @@ nr_positive_rewards = 0
 holdout_rewards = []
 prev_diff_cols_data = train[train.timestamp == max(train.timestamp)][['id'] + diff_cols].copy()
 
-# Predicting on holdout dataset day by day
+# Predicting on holdout dataset timestamp by timestamp
 while True:
     oidx += 1
     test = obs.features
